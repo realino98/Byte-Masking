839a2da338950b1b122d49cb42d769a883154f07
Bernard <bernardinus.r.w.k.j@polytron.co.id>
2022-06-28 15:19:14 +0700

    BR : Update

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 a = 0xf3 # 11110011
-print(bin(a))
+print("Source :", bin(a))
 
 #MSB -> 11110011 starts from the LEFT
+print("MSB ", end="")
 for bit in range(8):
     if (a & 0x80): #1000000
-        print(1)
+        print(1, end="")
     else:
-        print(0)
+        print(0, end="")
     a<<=1
 
+a = 0xf3 # 11110011
+print()
+print("Source :", bin(a))
 #LSB 11110011 <- starts from the RIGHT
+print("LSB ", end="")
 for bit in range(8): 
     if (a & 0x01): #0000001
-        print(1)
+        print(1, end="")
     else:
-        print(0)
+        print(0, end="")
     a>>=1
